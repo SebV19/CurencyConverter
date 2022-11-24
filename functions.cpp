93f981d1a0c7fb391f3b362ee7966fee1e1b019d
SebV19 <litoiu.sebastian19@gmail.com>
2022-11-24 19:12:12 +0200

    new find function

--- functions.cpp
+++ functions.cpp
@@ -1,56 +1,62 @@
 #include "functions.h"
 
-int hello(){
+void findMoney(fstream &file, string searchID, vector<string> storage, string &id, string &amount, string &country, string &currName)
+{
+    
+    file.open("Exchange.csv", ios::in);
+ 
+    bool found_currency = false;
+
+    while (getline (file, id, ',') && !found_currency)
+    {
+        getline (file, amount, ',');
+        getline (file, country, ',');
+        getline (file, country, '\n');
+
+            if(id == searchID)
+            {        
+                found_currency = true;
+                storage.push_back(id);
+                storage.push_back(amount);
+                storage.push_back(country);
+                storage.push_back(currName);
+            }
+ 
+    }
+    file.close();
+    found_currency = false;
+}
+
+
+int hello()
+{
 
     fstream MoneyList;
     MoneyList.open("Exchange.csv", ios::in);
 
     vector <string> row,row_comp;  
+
     string id, id_comp, amount, amount_comp, country, country_comp, currName, currName_comp;
     string search_id, search_id_comp;
 
-    int sell, buy;
+    int sell;
     bool found_currency = false;
 
+    cout<<"What is the currency that you want to exchange?"; cin >> search_id; cout<<endl;
+    cout<<"What currency do you want?";cin >> search_id_comp; cout<<endl;
+    cout<<"How much "<<search_id<<" do you have?";cin >> sell; cout<<endl;
 
-    cin>> search_id >> search_id_comp;
+    findMoney(MoneyList, search_id, row, id, amount, country, currName);
+    findMoney(MoneyList, search_id_comp, row_comp, id_comp, amount_comp, country_comp, currName_comp);
 
-    while (getline (MoneyList, id, ',') && !found_currency)
-    {
-            getline (MoneyList, amount, ',');
-            getline (MoneyList, country, ',');
-            getline (MoneyList, currName, '\n');
+    cout << row.size();
+    // float hamn = stof(amount);
+    // float bamn =  stof(amount_comp);
+    // float final = (sell * bamn)/ hamn;
+    // cout << id << country << endl;
+    // cout << id_comp<< country_comp << endl;
+    // cout << "( " << sell << " x " << bamn << ") / " << hamn << " " <<final<<endl;
+    // cout<< search_id << sell << " is equal to " << search_id_comp << " " << final <<" .";
 
-                 if(id == search_id)
-                {        
-                    found_currency = true;
-                    row.push_back(id);
-                    row.push_back(amount);
-                    row.push_back(country);
-                    row.push_back(currName);
-                }
-
-    }
-
-    found_currency = false;
-
-    while (getline (MoneyList, id_comp, ',') && !found_currency)
-    {
-            getline (MoneyList, amount_comp, ',');
-            getline (MoneyList, country_comp, ',');
-            getline (MoneyList, currName_comp, '\n');
-
-                 if(id_comp == search_id_comp)
-                {        
-                    found_currency = true;
-                    row_comp.push_back(id_comp);
-                    row_comp.push_back(amount_comp);
-                    row_comp.push_back(country_comp);
-                    row_comp.push_back(currName_comp);
-                }
-
-            }
-
-    cout<<"da";
     return 0;
 }
