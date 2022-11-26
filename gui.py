ab5d41571fcbb219a839a7593e0a6be114ee0064
SebV19 <litoiu.sebastian19@gmail.com>
2022-11-26 17:49:10 +0200

    start gui/bin/python3

--- gui.py
+++ gui.py
@@ -1,13 +1,16 @@
 import PySimpleGUI as pg
 
-pg.theme("DarkAmber")
+f = open("in.txt", 'w')
+
+pg.theme("DarkGreen5")
 
 layout = [
-    [pg.Text("Amount of money you want to exchange: ")],
-    [pg.InputText()],
+
     [pg.Text("Choose your Currency: ")],
     [pg.InputText()],
     [pg.Text("Choose the Currency you want to buy:")],
+    [pg.InputText()],
+    [pg.Text("Amount of money you want to exchange: ")],
     [pg.InputText()],
     [pg.Button("ok"), pg.Button("Cancel")]
 ]
@@ -18,6 +21,10 @@ while True:
     event, values = window.read()
     if event == "Cancel":
         break
-    print(values[0], values[1], values[2])
+    lines = [values[0], values[1], values[2]]
+    with open('in.txt', 'w') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
 
 window.close()
